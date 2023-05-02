d3b31ffb9ab1e1fb342f6d0ff6e58f2ea55cfb93
dimi1985 <xpilot88@hotmail.com>
2023-05-02 13:33:38 +0300

    Mongo

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const express = require('express');
-const app = express();
+const { MongoClient } = require('mongodb');
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Set up a MongoDB client and connect to the database
+const client = new MongoClient(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}.`);
+client.connect((err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log('Connected to MongoDB');
+
+  // Set up your API endpoints here using app.get(), app.post(), etc.
+
+  // Start the server
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
+});
+
+
+app.get('/api/users', async (req, res) => {
+  const db = client.db();
+  const users = await db.collection('users').find().toArray();
+  res.json(users);
 });
